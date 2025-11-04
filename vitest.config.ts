cceda5a6130978b7e919a2befae2f30a059c551f
sadan <117494111+sadan4@users.noreply.github.com>
2025-11-04 16:15:54 -0500

    spec.ts => test.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
-        include: ["test/**/*.spec.ts"],
+        include: ["test/**/*.test.?(c|m)[jt]s?(x)"],
         coverage: {
             enabled: true,
             include: ["src/**"],
